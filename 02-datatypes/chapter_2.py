ab4f94e91b69e6d574a8de0317c2dcab3d4b787a
atharvadhumal <atharvaadhumal@gmail.com>
2026-06-15 17:09:59 +0530

    learned about datatypes, set

---
+++ 02-datatypes/chapter_2.py
@@ -0,0 +1,10 @@
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+
+spice_mix.add("ginger")
+spice_mix.add("cinnamon")
+spice_mix.add("lemeon")
+
+print(f"After spice mix id: {id(spice_mix)}")
+print(f"After spice mix: {spice_mix}")
